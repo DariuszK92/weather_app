84fd4c9fdd68040a974d94776e8516b17090b8a2
DariuszK92 <60391491+DariuszK92@users.noreply.github.com>
2023-04-17 23:04:48 +0200

    Added Local storage for temp units and city chosen + current day weather style applied

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import _ from 'lodash';
 import './style.css';
 import background from './background.jpg'
-import { doc } from 'prettier';
-import getWeather from './websites/api';
+import storeWeather from './websites/api';
+import { showWeather } from './websites/api';
 
 
 
 const searchBtn = document.querySelector('#search');
+const unitToggle = document.querySelector('#checkbox');
+
+searchBtn.addEventListener('click', storeWeather);
 
 
+function unitChosen() {
+    let statusToggle = localStorage.getItem("toggleTempUnits");
+    if(statusToggle=='false' || statusToggle==null){
+        unitToggle.checked=false
+    } else if(statusToggle=='true') {
+        unitToggle.checked=true
+    }
+}
 
-searchBtn.addEventListener('click', getWeather)
+unitChosen();
+showWeather();
+
+function storeUnit() {
+    localStorage.setItem("toggleTempUnits", unitToggle.checked)
+}
+
+unitToggle.addEventListener('click', storeUnit)
--- src/websites/api.js
+++ src/websites/api.js
@@ -1,22 +1,48 @@
 const cityName = document.querySelector('#city-name');
 const errorPara = document.querySelector('.error');
 const cityInput = document.querySelector('#location');
+const currentTime = document.querySelector('#current-hour')
+const currentCondition = document.querySelector('#current-condition')
+const currentIcon = document.querySelector('#current-icon')
 
-export default async function getWeather() {
-    const citySearched = cityInput.value;
+
+export default function storeWeather() {
+    localStorage.setItem("cityChosen", cityInput.value);
+    showWeather();
+}
+
+
+ export async function showWeather() {
+    const citySearched = localStorage.getItem('cityChosen');
     const urlApi = `http://api.weatherapi.com/v1/current.json?key=b53f258c040d4673be784703231404&q=${citySearched}&aqi=no`
     try {
     const response = await fetch(urlApi);
     const currentData = await response.json();
-    cityName.innerHTML = currentData.location.name;
-    cityInput.value = '';
-    errorPara.innerHTML=''
-    console.log(currentData)
+    showCurrentStatus(currentData);
     }
     catch(error) {
-        errorPara.innerHTML = 'City you are searching does not exist, please try again'
-        cityInput.value = '';
-        cityName.innerHTML = ''
+        showError();
 
     }
 }
+
+
+function showError() {
+    errorPara.innerHTML = 'City you are searching does not exist, please try again'
+    cityInput.value = '';
+    cityName.innerHTML = ''
+}
+
+async function showCurrentStatus(currentData) {
+    cityName.innerHTML = currentData.location.name;
+    currentTime.innerHTML = currentData.location.localtime
+    currentCondition.innerHTML= currentData.current.condition.text;
+    let iconLength = await currentData.current.condition.icon.length;
+    let correctIcon = await currentData.current.condition.icon.slice(2, iconLength)
+    currentIcon.src  = await `http://${correctIcon}`;
+    cityInput.value = '';
+    errorPara.innerHTML='';
+    console.log(currentData)
+}
+
+
